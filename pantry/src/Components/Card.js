94536afbb6a9bf001fa176c0551b4980b8332df5
gnenov89 <georgi.nenov89@gmail.com>
2017-11-30 09:34:44 -0500

    struggle bus

--- pantry/src/Components/Card.js
+++ pantry/src/Components/Card.js
@@ -1,16 +1,15 @@
 import React,{ Component } from "react";
+import {Card} from "react-materialize";
 import CardTitle from "react-materialize"
 
-class Card extends Component {
-    render () {
-        return (
-            <Card className='small'
-            header={<CardTitle image='../assets/chef-min-icon.jpg'>Card Title</CardTitle>}
-            actions={[<a href="https://github.com/gnenov89/PantryChef/tree/mock-up-front-end/Mock-Up%20Front%20End">This is a Link</a>]}>
-            I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
-            </Card>
-        )
-    }
-}
+import {Button, Icon} from 'react-materialize'
 
-export default Card;
+export default  () => (
+    
+	<Card className='small'
+	header={<CardTitle image='img/sample-1.jpg'>Card Title</CardTitle>}
+	actions={[<a href='#'>This is a Link</a>]}>
+	I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
+</Card>
+
+);

9d0ba189420cb12dd1bf3ce9141a515026c64a4a
Georgi Nenov <geoergi.nenov89@gmail.com>
2017-11-29 22:11:10 -0500

    crashing bad

--- pantry/src/Components/navbar.js
+++ pantry/src/Components/navbar.js
@@ -1,22 +1,17 @@
 import React,{ Component } from "react";
+// import {Navbar} from "react-materialize"
+// import NavItem from "react-materialize"
+
 
 class Navbar extends Component {
     render () {
         return (
-            <div className="Navbar">
+            <div>
+                
 
-          
-                    <nav>
-                        <div class="nav-wrapper">
-                         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-                         <ul class="right hide-on-med-and-down">
-                         <li><a class="waves-effect waves-light btn">Log In<i class="material-icons right">arrow_right</i></a></li>
-                         </ul>
-                         <ul class="side-nav" id="mobile-demo">
-                         <li><a class="waves-effect waves-light btn">Log In<i class="material-icons right">arrow_right</i></a></li>
-                         </ul>
-                         </div>
-                    </nav>
+            
+           
+                    
            </div>
         )
     }

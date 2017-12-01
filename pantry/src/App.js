47d6bcb67b2bd6f839c2bafd1b9e24c80ecc5e41
gnenov89 <georgi.nenov89@gmail.com>
2017-11-30 21:11:16 -0500

    pages ready

--- pantry/src/App.js
+++ pantry/src/App.js
@@ -1,50 +1,33 @@
-import React,{ Component} from 'react';
-import Navbar from "./Components/Navbar.js";
-import Card from "./Components/Card.js";
-import Button from"./Components/Buttons.js";
+// BrowserRouter is the router implementation for HTML5 browsers (vs Native).
+// Link is your replacement for anchor tags.
+// Route is the conditionally shown component based on matching a path to a URL.
+// Switch returns only the first matching route rather than all matching routes.
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import React from 'react';
+import Navbar from './Components/Navbar'
+import HomeContainer from "./Components/Home/HomeContainer"
+import SearchContainer from "./Components/Search/SearchContainer"
+import "./App.css"
 
-import API from "./API";
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
 
-
-//import './App.css';
-
-class App extends Component {
-// eslint-disable-next-line
-  constructor(...args){
-    super(...args);
-    this.state = {
-    recipes :[],
-    open: true
-  };
-}
-  componentDidMount() {
-    API.searchRecipes("bread pudding")
-      .then(res => this.setState({ recipes: res.data.matches }));
-  }
-  // eslint-disable-next-line
-  render() {
-    console.log(this.state.recipes)
-    // eslint-disable-next-line
-    const{recipes} = this.state;
-    return (
-
- <div>
-
- <Navbar />
-
-  <Card />
-
-
-  <Button />
-
-
-</div>
-
-
-       
-    );
-
-  }
-}
+// We give each route either a target `component`, or we can send functions in `render` or `children` 
+// that return valid nodes. `children` always returns the given node whether there is a match or not.
+const App = () => (
+  <Router>
+    <div>
+      <Navbar/>
+      <Route exact path="/" component={HomeContainer} />
+      <Route exact path="/search" component={SearchContainer} />
+      <Route exact path="/about" component={About} />
+    </div>
+  </Router>
+);
 
 export default App;
--- pantry/src/Components/navbar.js
+++ pantry/src/Components/navbar.js
@@ -1,6 +1,6 @@
 import React,{ Component } from "react";
 import {Navbar, NavItem, Icon, } from "react-materialize"
-
+import {Link} from 'react-router-dom';
 
 
 class navbar extends Component {
@@ -12,9 +12,16 @@ class navbar extends Component {
     <Navbar brand='PantryChef'right>
     
     
-	<NavItem href='get-started.html'><Icon>search</Icon></NavItem>
-	<NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
-	<NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
+	  <NavItem href='get-started.html'><Icon><Link to="/search">search</Link></Icon></NavItem>
+    
+
+    
+	  <NavItem href='get-started.html'><Icon><Link to="/">view_module</Link></Icon></NavItem>
+    
+
+   
+	  <NavItem href='get-started.html'><Icon><Link to="/">refresh</Link></Icon></NavItem>
+    
 
    </Navbar>
          

54e0b319e372c3c0d007d3ce7f5e4af4d61d78d3
Georgi Nenov <geoergi.nenov89@gmail.com>
2017-12-05 21:10:01 -0500

    auth and footer finished

--- pantry/src/Components/footer.js
+++ pantry/src/Components/footer.js
@@ -1,5 +1,5 @@
 import React,{ Component } from "react";
-import {Footer} from "react-materialize"
+import {Footer, Icon} from "react-materialize"
 
 
 
@@ -7,13 +7,13 @@ class footer extends Component {
     render () {
         return (
             <div>
-            <Footer copyrights="&copy 2015 Copyright Text"
+            <Footer copyrights="&copy 2017 Pantry Chef"
 	moreLinks={
-		<a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-	}
+		<a className="grey-text text-lighten-4 right" href="https://github.com/gnenov89/PantryChef/tree/master/pantry"><Icon>code</Icon></a>
+}
 	links={
 		<ul>
-			<li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+			<li><a className="grey-text text-lighten-3" href="">Link 1</a></li>
 			<li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
 			<li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
 			<li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>

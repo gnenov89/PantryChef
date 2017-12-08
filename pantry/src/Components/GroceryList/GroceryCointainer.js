693e6484636dd0aa1231b6f9546aca9b3afa5222
Georgi Nenov <geoergi.nenov89@gmail.com>
2017-12-08 13:28:46 -0500

    text is looking good

---
+++ pantry/src/Components/GroceryList/GroceryCointainer.js
@@ -0,0 +1,19 @@
+import React,{ Component } from "react";
+import {} from "react-materialize"
+
+
+
+
+
+class Groceries extends Component {
+    render () {
+        return (
+            <div>
+
+           
+            </div>
+        )
+    }
+} 
+
+export default Groceries;

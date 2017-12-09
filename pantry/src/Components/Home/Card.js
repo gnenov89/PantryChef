8921bee7966fc64dc7ec98fed3c4a848e765f950
asimeoli <asimeoli@gmail.com>
2017-12-08 20:39:27 -0500

    :

--- pantry/src/Components/Home/Card.js
+++ pantry/src/Components/Home/Card.js
@@ -1,30 +1,31 @@
-import React,{ Component } from "react";
-import {Card, CardTitle, Row, Col} from "react-materialize";
-import logo from "./logo.jpg";
-
+import React, { Component } from "react";
+import { Card, CardTitle, Row, Col } from "react-materialize";
+import cardIcon from "./chef-icon-min.jpg";
+import icon from "./chef-icon-min.jpg";
 
 class card extends Component {
-    render () {
+    render() {
         return (
             <div>
-				<Row>
-                    <Col s={12} m={3} l={3} className="center-align"></Col>
+                <Row>
+                    <Col s={12} m={3} l={3} className="center-align" />
                     <Col s={12} m={6} l={6} className="center-align">
-                    <Card header={<CardTitle image={logo} width="25"/>}
-                    title={<p>Welcome to Pantry Chef!<br/>
-                        Here you can find your next homecooked meal by searching for the ingredients you have on hand.</p>}>
-                    </Card> 
-                    <Col s={12} m={6} l={6} className="left-align"></Col>
-                     
-                  
+                        <Card
+                            header={<CardTitle image={icon} />}
+                            title={
+                                <p>
+                                    Welcome to Pantry Chef! Here you can find
+                                    your next homecooked meal by searching for
+                                    the ingredients you have on hand.
+                                </p>
+                            }
+                        />
+                        <Col s={12} m={6} l={6} className="left-align" />
                     </Col>
-                    
-                </Row> 
-
-                
-           </div>
-        )
+                </Row>
+            </div>
+        );
     }
-} 
+}
 
 export default card;
--- pantry/src/Components/footer.js
+++ pantry/src/Components/footer.js
@@ -1,33 +1,28 @@
+
 import React,{ Component } from "react";
 import {Footer, Icon} from "react-materialize"
 
 
 
+
+
 class footer extends Component {
-    render () {
+    render() {
         return (
             <div>
-            <Footer copyrights="&copy 2017 Pantry Chef"
-	moreLinks={
-		<a className="grey-text text-lighten-4 right" href="https://github.com/gnenov89/PantryChef/tree/master/pantry"><Icon>code</Icon></a>
-}
-	links={
-		<ul>
-			<li><a className="grey-text text-lighten-3" href="">Link 1</a></li>
-			<li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-			<li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-			<li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-		</ul>
-	}
-	className='example'
->
-		<h5 className="white-text">Footer Content</h5>
-		<p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-</Footer>
-            </div>
 
-        )
+
+                <Footer copyrights="Copyright &copy; 2017 Pantry Chef" className="example">
+                    <a className="grey-text text-lighten-4 right"
+                        href="https://github.com/gnenov89/PantryChef/tree/master/pantry"
+                    >
+                        
+                    </a>
+                </Footer>
+
+            </div>
+        );
     }
-} 
+}
 
 export default footer;

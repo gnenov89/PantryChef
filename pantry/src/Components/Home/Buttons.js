58775a494d81354e6c93d86d6c1feb1e849a7812
asimeoli <asimeoli@gmail.com>
2017-12-08 21:41:18 -0500

    got the sticky footer stuck

--- pantry/src/Components/Home/Buttons.js
+++ pantry/src/Components/Home/Buttons.js
@@ -1,43 +1,42 @@
-import React,{ Component } from "react";
-import {Button, Row, Col, Icon} from "react-materialize"
-import {Link} from 'react-router-dom';
-
+import React, { Component } from "react";
+import { Button, Row, Col, Icon } from "react-materialize";
+import { Link } from "react-router-dom";
 
 class buttons extends Component {
-    render () {
+    render() {
         return (
             <div>
-
-            <Row>
-
-                <Col s={12}className = "center">
-                  <Link to="/ingredientsearch"><Button waves='light'>Search By Ingredient </Button></Link>
-                </Col>
+                <Row>
+                    <Col s={12} className="center">
+                        <Link to="/ingredientsearch">
+                            <Button waves="light">Search By Ingredient </Button>
+                        </Link>
+                    </Col>
                 </Row>
                 <Row>
-
-                <Col s={12} className='center'>
-                  <Button waves='light' node='a' href='/SearchByRecipe'> Search By Recipe </Button>
-                </Col>
-                
+                    <Col s={12} className="center">
+                        <Button waves="light" node="a" href="/SearchByRecipe">
+                            {" "}
+                            Search By Recipe{" "}
+                        </Button>
+                    </Col>
                 </Row>
-                <Row>
-                <Col s={3}></Col>
-                <Col s={3} >
-    		    <Button className='right' waves='light'>list<Icon left>list</Icon></Button>
-                </Col>
-              
-                <Col s={3}>
-                  <Link to={{pathname: '/about'}}>
-                    <Button className='left' waves='light'>Favorites<Icon left>favorite</Icon></Button>
-                  </Link>
-                </Col>
-                <Col s={3}></Col>
 
-            </Row>
-           </div>
-        )
+                <Row>
+                    <Col s={12} className="center">
+                        <Button
+                            className="center"
+                            node="a"
+                            href="/GroceryList"
+                            waves="light"
+                        >
+                            list<Icon left>list</Icon>
+                        </Button>
+                    </Col>
+                </Row>
+            </div>
+        );
     }
-} 
+}
 
 export default buttons;
